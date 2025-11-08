2bfeeb93764d8beff10a59d3a8ca23d6b3a9650d
vcque <vincent.ocquet@gmail.com>
2025-11-08 12:39:39 +0100

    feat(wasm): Attribute a random id if none is in the search params

--- crates/client-wasm/src/main.rs
+++ crates/client-wasm/src/main.rs
@@ -3,15 +3,15 @@ use std::{
     sync::{Arc, Mutex},
 };
 
+use crate::{ratzilla_adapter::RatzillaAdapter, ws::WsServer};
 use log::Level;
 use losig_client::{game::GameSim, tui::GameTui};
 use losig_core::{
     network::{UdpCommandPacket, UdpSensesPacket},
     types::AvatarId,
 };
-use web_sys::{UrlSearchParams, window};
-
-use crate::{ratzilla_adapter::RatzillaAdapter, ws::WsServer};
+use wasm_bindgen::JsValue;
+use web_sys::{Url, UrlSearchParams, window};
 
 mod ratzilla_adapter;
 mod ws;
@@ -22,7 +22,8 @@ pub type CommandMessage = UdpCommandPacket;
 fn main() -> io::Result<()> {
     console_log::init_with_level(Level::Debug).unwrap();
 
-    let id = get_avatar_id().unwrap_or(1);
+    let id = get_avatar_id().unwrap_or_else(|| generate_avatar_id());
+    update_history(id);
 
     let game = GameSim::new(id);
     let game = Arc::new(Mutex::new(game));
@@ -61,4 +62,31 @@ fn get_avatar_id() -> Option<AvatarId> {
     let params = UrlSearchParams::new_with_str(&params).ok()?;
 
     params.get("id").and_then(|s| s.parse::<AvatarId>().ok())
+}
+
+fn generate_avatar_id() -> AvatarId {
+    let crypto = window().unwrap().crypto().unwrap();
+    let mut array = [0u8; 4];
+    crypto.get_random_values_with_u8_array(&mut array).unwrap();
+    u32::from_le_bytes(array)
+}
+
+/// Change the id in the url so that the player can bookmark its player (will need proper user
+/// manager on day)
+fn update_history(id: AvatarId) {
+    let window = window().unwrap();
+    let location = window.location();
+
+    let current_url = location.href().unwrap();
+
+    let url = Url::new(&current_url).unwrap();
+    let search_params = url.search_params();
+    search_params.set("id", &format!("{id}"));
+
+    url.set_search(&search_params.to_string().as_string().unwrap());
+
+    let history = window.history().unwrap();
+    history
+        .push_state_with_url(&JsValue::NULL, "", Some(&url.href()))
+        .unwrap();
 }
